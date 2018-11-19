f32b2fcdc2dc9afb412e05b53af6cbfee52c5802
joshuakessell <joshuakessell@icloud.com>
2018-11-18 19:11:05 -0600

    uploading heroku changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect('mongodb://localhost/tiny_improvements', { useNewUrlParser: true });
+mongoose.connect('mongodb://jckessell:Ranch5tyl3@ds211724.mlab.com:11724/heroku_js4qm9h5', { useNewUrlParser: true });
 
 
 require('./routes/api-routes')(app);
